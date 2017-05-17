225eb2f5e46c7d61de9c665a0b9ace30aa55936f
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-05-16 23:12:03 -0600

    basic layout, pseudocode, and random character array generation

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var characters = ["Bob", "Linda", "Tina", "Gene", "Louise", "Andy", "Ollie", "Tammy", "Zeke", "Mickey"];
+var wins = 0;
+var guess;
+
+var currentWord = characters[Math.floor(Math.random() * characters.length)];
+
+
+
+function addWins() {
+document.getElementById("wins").innerHTML = "Wins = " + wins++;
+
+}
+document.onkeyup = function guesses() {
+	var guess = event.key;
+    document.getElementById("lettersGuessed").innerHTML = + guess;
+}
+
+console.log(characters[4]);
+
