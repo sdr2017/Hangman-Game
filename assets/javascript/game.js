58afd9094e336240a203cbffb00901377ed29f1a
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-05-17 22:02:49 -0600

    fixing all the things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+
+
 //Array of characters computer can generate
 var characters = ["Bob", "Linda", "Tina", "Gene", "Louise", "Andy", "Ollie", "Tammy", "Zeke", "Mickey"];
 var wins = 0;
-var guess;
 
-var currentWord = characters[Math.floor(Math.random() * characters.length)];
+//this is an empty array
+var guess = [];
 
+var countdown = 15;
 
+//Computer randomly generating word from character array
 
-function addWins() {
-document.getElementById("wins").innerHTML = "Wins = " + wins++;
-
+var currentWord;
+function wordGenerate() {
+	currentWord = characters[Math.floor(Math.random() * characters.length)];
+	document.getElementById("puzzle").innerHTML = currentWord;
+	console.log(currentWord);
 }
+wordGenerate();
 
-function addWins() {
-document.getElementById("wins").innerHTML = "Wins = " + wins++;
 
-}
-document.onkeyup = function guesses() {
-	var guess = event.key;
+
+
+//Computer displaying the guessed letters
+document.onkeyup = function(event) {
+
+	//event.key containts the letter pressed
+	//.push means to push it into the array "guess"
+	guess.push(event.key);
     document.getElementById("lettersGuessed").innerHTML = guess;
+    countdown--;
+    document.getElementById("countdown").innerHTML = "Number of guesses remaining " + countdown;
+
 }
 
-console.log(characters[4]);
+//trying to countdown number of guesses leftq
 
+
+function addWins() {
+document.getElementById("wins").innerHTML = "Wins = " + wins++;
+
+}
+
+});
